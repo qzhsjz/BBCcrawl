1c8c83b5e947fa60f38d14542955f911b540b422
Angelo Qin <qzhsjz@gmail.com>
2017-02-15 21:41:01 +0800

    增加多种内容页面的支持

--- BBCcrawl/spiders/bbc.py
+++ BBCcrawl/spiders/bbc.py
@@ -23,22 +23,69 @@ class BbcSpider(scrapy.Spider):
             # for para in response.xpath('//div[contains(@property, "articleBody")]/p'):
             #     print(para.xpath('text()'))
 
-            # 处理标题
-            item['Title'] = response.xpath('//h1[contains(@class, "story-body__h1")]/text()').extract()[0]
-            # 处理主体
-            body = response.xpath('//div[contains(@property, "articleBody")]/p/text()').extract()
-            item['Summary'] = body[0]
-            item['Content'] = '\n'.join(body[1:])
+            # 决定是否留存item的标记
+            self.gooditem = True
+
+            # 处理文章标题
+            try:
+                item['Title'] = response.xpath('//h1[contains(@class, "story-body__h1")]/text()').extract()[0]
+            except IndexError:
+                # 丢弃文章
+                self.gooditem = False
+
+            # 处理文章主体
+            # 正常模式
+            try:
+                body = response.xpath('//div[contains(@property, "articleBody")]/p/text()').extract()
+                item['Summary'] = body[0]
+                item['Content'] = '\n'.join(body[1:])
+            except IndexError:
+                # 视频模式
+                try:
+                    body = response.xpath('//div[contains(@id, "media-asset-page-text")]/div/p/text()').extract()
+                    item['Summary'] = ''
+                    item['Content'] = '\n'.join(body)
+                except IndexError:
+                    # 丢弃文章
+                    self.gooditem = False
+
             # 处理分类
             # 旧版代码：item['Type'] = response.url.lstrip('http://www.bbc.com/news/').split('/')[0].split('-')[0]
-            item['Type'] = response.xpath('//a[contains(@data-entityid, "section-label")]/text()').extract()[0]
-            # 处理网址
+            # 方法1：使用文章内部的分类标志
+            try:
+                item['Type'] = response.xpath('//a[contains(@data-entityid, "section-label")]/text()').extract()[0]
+            except IndexError:
+                # 方法2：使用导航栏上被选中的分类标志
+                try:
+                    item['Type'] = response.xpath(
+                        '//a[contains(@class, "selected")][1]/span/text() | //li[contains(@class, "selected")][1]/a/span/text()').extract()[
+                        0]
+                except IndexError:
+                    # 方法3：从URL中提取
+                    try:
+                        item['Type'] = response.url.split('/news/')[1].split('/')[-1].rstrip('-1234567890')
+                    except IndexError:
+                        # 疑难杂类
+                        item['Type'] = ''
+
+            # 处理 Reference URL
             item['Refer'] = response.url
+
             # 处理发布时间
-            item['Pubtime'] = int(
-                response.xpath('//li[contains(@class, "mini-info-list__item")]/div/@data-seconds').extract()[0])
-            # 将爬取好的文章对象扔出去
-            yield item
+            try:
+                item['Pubtime'] = int(
+                    response.xpath('//li[contains(@class, "mini-info-list__item")]/div/@data-seconds').extract()[0])
+            except IndexError:
+                try:
+                    item['Pubtime'] = int(response.xpath('//p[contains(@class, "date")][1]/@data-seconds').extract()[0])
+                except IndexError:
+                    # 丢弃文章
+                    self.gooditem = False
+
+            # 将爬取好的文章对象yield
+            if self.gooditem: yield item
+
+            # 处理其它链接
             # 对延伸阅读的处理（一）——使用文末的more on this story
             for story in response.xpath('//div[contains(@class, "more-on-this-story")]'):
                 for linkedstory in story.xpath('div/ul/li'):
@@ -50,6 +97,7 @@ class BbcSpider(scrapy.Spider):
                         yield scrapy.Request(url=UrlItem['URL'])
                     except IndexError:
                         pass
+
             # 对延伸阅读的处理（二）——使用文中插入的Read More
             for story in response.xpath('//a[contains(@class, "story-body__link")]'):
                 UrlItem['URL'] = story.xpath('@href').extract()[0]
@@ -57,6 +105,8 @@ class BbcSpider(scrapy.Spider):
                 if UrlItem['URL'][0:6] != 'http://':
                     UrlItem['URL'] = 'http://www.bbc.com' + UrlItem['URL']
                 yield scrapy.Request(url=UrlItem['URL'])
+
+
         # 对栏目主页的处理
         elif response.url.lstrip('http://www.bbc.com/news/').split('/')[0] in NewsTypeList:
             for link in response.xpath('//a[contains(@class, "title-link")]'):
@@ -68,6 +118,8 @@ class BbcSpider(scrapy.Spider):
                 # print('抓取到【栏目主页】中的新闻稿链接：')
                 # print(UrlItem)
                 yield scrapy.Request(url=UrlItem['URL'])
+
+
         # 对新闻首页的处理
         elif response.url[-4:] == 'news':
             for link in response.xpath('//a[contains(@class, "navigation-wide-list__link")]'):
@@ -79,7 +131,9 @@ class BbcSpider(scrapy.Spider):
                 # print('抓取到【新闻首页】中的栏目链接：')
                 # print(UrlItem)
                 yield scrapy.Request(url=UrlItem['URL'])
-        # 主页的处理
+
+
+        # BBC主页的处理
         else:
             for link in response.xpath('//a[contains(@class, "media__link")]'):
                 UrlItem = BbccrawlURLItem()
